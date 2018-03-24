93770f5f27f6ff11353a184b3f114385f45b1912
cwong2015 <sirhc8002@gmail.com>
2018-03-24 12:11:56 -0400

    fool proofed q3 p2

--- q3_p2.pl
+++ q3_p2.pl
@@ -8,9 +8,8 @@ $curr_currency = <STDIN>;
 chomp $curr_currency;
 
 while(not $hashf{$curr_currency}){
-	print "Invalid currency!\n";
-	print "Exchangeable currency: usd, eur, cad, inr, jpy, vnd, krw, btc\n";
-	print "Enter the current currency: ";
+	print "We do not trade $curr_currency!\n";
+	print "Re-enter the current currency: ";
 	$curr_currency = <>;
 	chomp $curr_currency;
 }
@@ -22,9 +21,8 @@ $target_currency = <STDIN>;
 chomp $target_currency;
 
 while(not $hashf{$target_currency}){
-	print "Invalid currency!\n";
-	print "Exchangeable currency: usd, eur, cad, inr, jpy, vnd, krw, btc\n";
-	print "Enter the target currency: ";
+	print "We do not trade $target_currency!\n";
+	print "Re-enter the target currency: ";
 	$target_currency = <>;
 	chomp $target_currency;
 }
@@ -39,5 +37,11 @@ $ratio = ($hashf{$curr_currency})/($hashf{$target_currency});
 $converted = $amount/$ratio;
 
 print "$amount $curr_currency is ";
-printf("%.2f", $converted);
-print " $target_currency";
+if(int($converted)!=$converted){
+	printf("%.2f", $converted);
+}
+else{
+	print($converted);
+}
+
+print " $target_currency.";
