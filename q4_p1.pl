a45ccbae51da978ae1d72df28f42cb801bf287a0
cwong2015 <sirhc8002@gmail.com>
2018-03-24 15:42:36 -0400

    q4 p1 annoying af

--- q4_p1.pl
+++ q4_p1.pl
@@ -1,8 +1,11 @@
+use File::Copy;
 print "Enter a file name: ";
 $file_name = <>;
 chomp $file_name;
 
 $string = "Perl is cool!";
+
+@lines = ();
 
 # if file is present go inside...
 if(-e $file_name){
@@ -23,9 +26,11 @@ if(-e $file_name){
 	}	
 	
 	# now that a backup directory has been created, check if file has 10 lines
-	open LINES, "+>$file_name";
+	open LINES, "+<$file_name";
 	$lines = 0;
-	while(($line = <LINES>)){
+	while($line = <LINES>){
+		push @lines, $line if($lines < 10);
+		push @lines_2, $line;
 		$lines++;
 	}
 	if($lines > 10){
@@ -34,18 +39,32 @@ if(-e $file_name){
 		$userOption = <>;
 		chomp $userOption;
 		if($userOption eq "o"){
-			print LINES $string;
+			seek(LINES, 0, 0);
+			truncate LINES, 0;
+			print LINES "$string";
 			print "Wrote to file $file_name";
 		}
 		elsif($userOption eq "c"){
+			open COPY, ">backup/$file_name";
+			print COPY @lines;
 			print "Ok, old file backed up under backup directory\n";
+			seek(LINES, 0, 0);
+			truncate LINES, 0;
+			print LINES "$string";
 			print "Wrote to file $file_name";
+			close COPY;
 		}
 	}
 	else{
 		print "$file_name has no more than 10 lines.\n";
+		open COPY, ">backup/$file_name";
+		print COPY @lines;
 		print "Ok, old file backed up under backup directory\n";
+		seek(LINES, 0, 0);
+		truncate LINES, 0;
+		print LINES "$string";
 		print "Wrote to file $file_name";
+		close COPY;
 	}
 	close LINES;
 }
